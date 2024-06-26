27296f35291feabf763b5af753d9b3871291dc68
Alex <alexgithinji44@gmail.com>
2024-06-26 14:36:36 +0300

    added the first migration

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ config()
 const createproducts = async () => {
    const products = await prisma.products.create({
         data: {
-            productTitle : "Unsweet",
-            productThumbnail : "http://dummyimage.com/240x100.png/cc0000/fff0ff",
+            productTitle : "Flavor",
+            productThumbnail : "http://image.com/240x100.png/cc0000/fff0ff",
             productDescription : "sapien arcu sed augue aliquam erat volutpat in congue etiam justo etiam pretium iaculis justo in hac habitasse platea dictumst etiam faucibus cursus urna ut tellus nulla ut erat id mauris vulputate elementum nullam varius nulla facilisi cras non velit nec nisi vulputate nonummy maecenas tincidunt lacus at velit vivamus",
-            productCost : 20,
+            productCost : 150,
             onOffer : false
         }
     })
